a98f4f316beffa9c319fb448fab6f309e088fab2
Harvey Sison <harveyjaysison@gmail.com>
2021-02-26 17:01:04 +0800

    complete

---
+++ lib/utils/model.dart
@@ -0,0 +1,8 @@
+class TweetItemModel {
+  String tweet;
+  String username;
+  String time;
+  String twitterHandle;
+
+  TweetItemModel(this.tweet, this.username, this.time, this.twitterHandle);
+}
